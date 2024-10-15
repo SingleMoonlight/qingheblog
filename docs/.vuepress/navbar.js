e6a92d899929381bafb53ebf0067bbba9ad1910f
SingleMoonlight <winston.wyx@foxmail.com>
2024-10-15 23:26:23 +0800

    Add icon for nav bar

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -1,12 +1,13 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: '标签', link: '/tags/' },
-  { text: '分类', link: '/categories/' },
-  { text: '归档', link: '/archives/' },
+  { text: '首页', link: '/', icon: 'material-symbols:home'},
+  { text: '标签', link: '/tags/', icon: 'mdi:tag' },
+  { text: '分类', link: '/categories/', icon: 'dashicons:category' },
+  { text: '归档', link: '/archives/', icon: 'ph:file-archive-fill' },
   {
     text: '笔记',
+    icon: 'solar:notebook-bold',
     items: [
       { text: 'C编程', link: '/c/', activeMatch: '^/c/', },
       { text: 'Java编程', link: '/java/', activeMatch: '^/java/', },
